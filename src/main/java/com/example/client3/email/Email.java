ae1bc027e44e698ead2511c4baf3e1538d3cb987
AnastasiiaTitkova <128168129+AnastasiiaTitkova@users.noreply.github.com>
2023-03-19 15:20:14 +0400

    result client3 2

--- src/main/java/com/example/client3/email/Email.java
+++ src/main/java/com/example/client3/email/Email.java
@@ -1,2 +1,14 @@
-package com.example.client3.email;public class Email {
+package com.example.client3.email;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Email {
+    @GetMapping("/email")
+    public String sendPush(){
+        System.out.println("email was sent");
+        return "email was sent";
+    }
 }
