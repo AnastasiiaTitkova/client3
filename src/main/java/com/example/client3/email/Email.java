1589ab2dd38845b97b8f638ed36e2c182bd5fa2e
AnastasiiaTitkova <128168129+AnastasiiaTitkova@users.noreply.github.com>
2023-03-20 10:22:44 +0400

    result

--- src/main/java/com/example/client3/email/Email.java
+++ src/main/java/com/example/client3/email/Email.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Email {
-    @RequestMapping(value="/email", method= RequestMethod.POST)
+    @PostMapping("/email")
     public String sendPush(){
         System.out.println("email was sent");
         return "email was sent";
